9a56193d06c00233a65efe0ff16efb0b3188197a
choongnyeong6215 <choong6215@gmail.com>
2023-12-19 16:31:22 +0900

    231219 홈화면 작업 완료

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,53 @@
+import { useEffect, useState } from "react";
+import { Container, Header, Title, CoinsList, Coin, Loading, CoinImg } from "../styles/homeStyle"
+import { Link } from "react-router-dom";
+
+// api 넘겨받을 데이터 타입 지정
+interface CoinItfc {
+  id : string;
+  name : string;
+  symbol : string;
+  rank : number;
+  is_new : boolean;
+  is_active : boolean;
+  type : string;
+}
+
 const Home = () => {
+  // 코인 리스트
+  const [coinList, setCoinList] = useState<CoinItfc[]>([]);
+  // 로딩 관리
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/coins")
+    .then((response) => response.json())
+    .then((data) => setCoinList(data.slice(0, 100)))
+    setLoading(false);
+  }, []);
+
+  console.log(coinList);
+  
   return (
     <div>
-      <h1>Coin Tracker!</h1>
+      <Container>
+        <Header>
+          <Title>Coin Tracker</Title>
+        </Header>
+        {loading ? <Loading>Loading...</Loading> : (
+          <CoinsList>
+            {coinList.map((coin) => (
+              <Coin key={coin.id}>
+                  <CoinImg src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} alt="coinImage" />
+                  <Link to={`/${coin.id}`}>
+                    {coin.name}
+                  </Link>
+              </Coin>
+            ))}
+          </CoinsList>
+        ) 
+      }
+      </Container>
     </div>
   )
 }
---
+++ src/styles/homeStyle.tsx
@@ -0,0 +1,67 @@
+import styled, { keyframes } from "styled-components";
+
+export const Container = styled.div`
+    max-width: 500px;
+    margin: 0rem auto;
+`;
+
+export const Header = styled.header`
+    display : flex;
+    justify-content : center;
+    align-items : center;
+    height : 20vh;
+`;
+
+export const Title = styled.h1`
+font-size : 3rem;
+`;
+
+export const CoinsList = styled.ul`
+    display : grid;
+    grid-template-columns : repeat(4, 1fr);
+    gap : 1rem;
+`;
+
+export const Coin = styled.li`
+    width: 120px;
+    height: 120px;
+    border-radius : 1rem;
+    background-color : white;
+    display : flex;
+    flex-direction: column;
+    justify-content : center;
+    align-items : center;
+    a {
+        color : ${(props) => props.theme.bgColor};
+        text-align : center;
+        margin-top: 1rem;
+        font-size: 0.75rem;
+        &:hover {
+            cursor : pointer;
+            color : ${(props) => props.theme.empTxtColor};
+        }
+    }
+`;
+
+export const CoinImg = styled.img`
+    width: 50px;
+    height: 50px;
+`;
+
+export const LoadingAnimation = keyframes`
+    0% {
+        opacity: 1;
+    }
+    100% {
+        opacity: 0;
+    }
+`;
+
+export const Loading = styled.h1`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 5rem;
+    animation: ${LoadingAnimation} 2s linear infinite;
+`;
