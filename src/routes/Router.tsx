28dc5623907bf6789f0080b592dc07d6d074ebb5
choongnyeong6215 <choong6215@gmail.com>
2024-04-12 21:55:23 +0900

    배포 경로 수정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,22 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
 import Coin from "../components/Coin";
-import Price from "../components/Price"
-import Chart from '../components/Chart';
+import Price from "../components/Price";
+import Chart from "../components/Chart";
 
 const Router = () => {
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home/> } />
-            <Route path="/:coinId/*" element={<Coin/> } >
-              {/* 중첩 라우팅  */}
-              <Route path="price" element={<Price /> } />
-              <Route path="chart" element={<Chart /> } />
-            </Route>
-        </Routes>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:coinId/*" element={<Coin />}>
+          {/* 중첩 라우팅  */}
+          <Route path="price" element={<Price />} />
+          <Route path="chart" element={<Chart />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
